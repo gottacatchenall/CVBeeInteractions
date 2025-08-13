ce5629a5fc4ba02098f15d324a1b37db49689223
Michael D. Catchen <mdcatchen@gmail.com>
2025-08-13 13:39:46 -0400

    sigh

--- compile_tests.py
+++ compile_tests.py
@@ -6,6 +6,8 @@ from transformers import AutoModel
 import time
 import os
 
+
+"""
 # -------------------------------
 # 1. Load model and compile
 # -------------------------------
@@ -28,12 +30,29 @@ print(f"Compile time: {time.time() - starttime} seconds")
 # 2. Warmup with a small dummy batch
 # -------------------------------
 starttime = time.time()
-print(f"Dummy batch time: {time.time() - starttime} seconds")
 dummy = torch.randn(2, 3, 224, 224, device="cuda")
 _ = model(dummy)  # triggers kernel compilation / fusion
 
-print(f"Compile time {time.time() - starttime}")
+print(f"Dummy batch time: {time.time() - starttime} seconds")
 
 
 model_path = os.path.join("/scratch", "mcatchen", "precompiled_vit.pt")
 torch.save(model, model_path)
+"""
+# load that bad boy
+
+model_path = os.path.join("/scratch", "mcatchen", "precompiled_vit.pt")
+model = torch.load(model_path, weights_only=False)
+
+starttime = time.time()
+dummy = torch.randn(2, 3, 224, 224, device="cuda")
+_ = model(dummy)  # triggers kernel compilation / fusion
+print(f"Dummy batch time: {time.time() - starttime} seconds")
+
+starttime = time.time()
+for i in range(64):
+    starttime = time.time()
+    dummy = torch.randn(2, 3, 224, 224, device="cuda")
+    _ = model(dummy)  # triggers kernel compilation / fusion
+
+print(f"Multi dummy batch time: {time.time() - starttime} seconds")
