7f354192b4c0bad3fe518ab185b65c83e3c8cd28
Michael D. Catchen <mdcatchen@gmail.com>
2025-08-14 14:21:06 -0400

    move metrics outside grad

--- lightning.py
+++ lightning.py
@@ -80,92 +80,94 @@ class SpeciesImageDataModule(pl.LightningDataModule):
             self.predict = SpeciesImageDataset(self.data_dir, train=False, transform=self.transform)
 
     def train_dataloader(self):
-        return DataLoader(self.train, batch_size=self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.train, batch_size=self.batch_size, num_workers=self.num_workers, drop_last=True)
 
     def val_dataloader(self):
-        return DataLoader(self.val, batch_size=self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.val, batch_size=self.batch_size, num_workers=self.num_workers, drop_last=True)
 
     def test_dataloader(self):
-        return DataLoader(self.test, batch_size=self.batch_size, num_workers=self.num_workers)
+        return DataLoader(self.test, batch_size=self.batch_size, num_workers=self.num_workers, drop_last=True)
+    
+class ViTSpeciesEmbeddingModel(pl.LightningModule):
+    def __init__(
+        self, 
+        num_classes=19,
+        species_embedding_dim = 128, 
+    ):
+        super(ViTSpeciesEmbeddingModel, self).__init__()
+        self.loss_function = nn.CrossEntropyLoss()
+        self.num_classes = num_classes
+        self.species_embedding_dim = species_embedding_dim
+
+        self.image_model = AutoModel.from_pretrained(
+            "google/vit-base-patch16-224", 
+            local_files_only=True
+        )
+
+        # --- Freeze ViT layers  ---
+        for param in self.image_model.embeddings.parameters():
+            param.requires_grad = False
+        for param in self.image_model.encoder.parameters():
+            param.requires_grad = False
+        for param in self.image_model.pooler.parameters():
+            param.requires_grad = True
+        
+        self.embedding_model = nn.Linear(
+            768, 
+            species_embedding_dim
+        )
+        self.classification_head = nn.Sequential(
+            nn.ReLU(),
+            nn.Linear(species_embedding_dim, num_classes) 
+        )
+
+        self.train_metrics = torchmetrics.MetricCollection(
+            {
+                "accuracy": torchmetrics.classification.Accuracy(
+                    task="multiclass", 
+                    num_classes=19
+                ),
+                "MAP_macro": torchmetrics.classification.MulticlassPrecision(num_classes=19, average='macro'),
+                "MAP_micro": torchmetrics.classification.MulticlassPrecision(num_classes=19, average='micro'),
+            },
+            prefix="train_",
+        )
+        self.valid_metrics = self.train_metrics.clone(prefix="valid_")
+
+    def forward(self, x):
+        outputs = self.image_model(x, output_hidden_states=False, output_attentions=False)
+        pooled = outputs.pooler_output
+        return self.classification_head(self.embedding_model(pooled))
+    
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.forward(x)
+        loss = F.cross_entropy(y_hat, y)
+
+        with torch.no_grad():
+            batch_value = self.train_metrics(y_hat, y)
+            self.log_dict(batch_value)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self.forward(x)
+        with torch.no_grad():
+            batch_value = self.valid_metrics(y_hat, y)
+            self.log_dict(batch_value)
+            #val_loss = F.cross_entropy(y_hat, y)
+            #batch_value["valid_loss"] = val_loss
+
+    def on_train_epoch_end(self):
+        self.train_metrics.reset()
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=args.lr)
 
 
 def main(args):
     print("Starting...")
 
-    
-    
-
-    class ViTSpeciesEmbeddingModel(pl.LightningModule):
-        def __init__(
-            self, 
-            num_classes=19,
-            species_embedding_dim = 128, 
-        ):
-            super(ViTSpeciesEmbeddingModel, self).__init__()
-            self.loss_function = nn.CrossEntropyLoss()
-            self.num_classes = num_classes
-            self.species_embedding_dim = species_embedding_dim
-
-            self.image_model = AutoModel.from_pretrained(
-                "google/vit-base-patch16-224", 
-                local_files_only=True
-            )
-
-            # --- Freeze ViT layers  ---
-            for param in self.image_model.embeddings.parameters():
-                param.requires_grad = False
-            for param in self.image_model.encoder.parameters():
-                param.requires_grad = False
-            for param in self.image_model.pooler.parameters():
-                param.requires_grad = True
-            
-            self.embedding_model = nn.Linear(
-                768, 
-                species_embedding_dim
-            )
-            self.classification_head = nn.Sequential(
-                nn.ReLU(),
-                nn.Linear(species_embedding_dim, num_classes) 
-            )
-
-            self.train_metrics = torchmetrics.MetricCollection(
-                {
-                    "accuracy": torchmetrics.classification.Accuracy(
-                        task="multiclass", 
-                        num_classes=19
-                    ),
-                    "MAP_macro": torchmetrics.classification.MulticlassPrecision(num_classes=19, average='macro'),
-                    "MAP_micro": torchmetrics.classification.MulticlassPrecision(num_classes=19, average='micro'),
-                },
-                prefix="train_",
-            )
-            self.valid_metrics = self.train_metrics.clone(prefix="valid_")
-
-        def forward(self, x):
-            return self.classification_head(self.embedding_model(self.image_model(x).pooler_output))
-        
-        def training_step(self, batch, batch_idx):
-            x, y = batch
-            y_hat = self.forward(x)
-            loss = F.cross_entropy(y_hat, y)
-            batch_value = self.train_metrics(y_hat, y)
-            self.log_dict(batch_value)
-            return loss
-        
-        def validation_step(self, batch, batch_idx):
-            x, y = batch
-            y_hat = self.forward(x)
-            batch_value = self.valid_metrics(y_hat, y)
-            #val_loss = F.cross_entropy(y_hat, y)
-            #batch_value["valid_loss"] = val_loss
-            self.log_dict(batch_value)
-
-        def on_train_epoch_end(self):
-            self.train_metrics.reset()
-
-        def configure_optimizers(self):
-            return torch.optim.Adam(self.parameters(), lr=args.lr)
-    
     feature_extractor = AutoFeatureExtractor.from_pretrained("google/vit-base-patch16-224", local_files_only=True, use_fast=True)
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -187,7 +189,7 @@ def main(args):
     )
 
     model = ViTSpeciesEmbeddingModel()
-    compiled_model = torch.compile(model)
+    compiled_model = torch.compile(model, mode="reduce-overhead")
 
   
     num_gpus = torch.cuda.device_count()
