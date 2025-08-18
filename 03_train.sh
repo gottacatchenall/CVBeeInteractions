f025de78e65027f1961c54b8d557b2f58737ef34
Michael D. Catchen <mdcatchen@gmail.com>
2025-08-18 14:30:48 -0400

    async checkpoint test

--- 03_train.sh
+++ 03_train.sh
@@ -2,11 +2,11 @@
 #SBATCH --nodes 1             
 #SBATCH --gres=gpu:1  
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --mem=16G      
-#SBATCH --time=01:00:00
+#SBATCH --time=00:15:00
 #SBATCH --output=%x-%j.out
-#SBATCH --job-name=DINOv3Test
+#SBATCH --job-name=AsyncCheckptTest
 
 module load python/3.13
 virtualenv --no-download $SLURM_TMPDIR/env
@@ -14,4 +14,4 @@ source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index -r requirements.txt
 export TORCH_NCCL_ASYNC_HANDLING=1
 
-srun python 03_vit_test.py --batch_size 128 --cluster --max_epochs 100 --species plants --lr 3e-4 --num_workers 8 --persistent_workers --prefetch_factor 8 --model large
+srun python 03_vit_test.py --batch_size 128 --cluster --max_epochs 100 --species plants --lr 3e-4 --num_workers 4 --persistent_workers --prefetch_factor 8 --model large
