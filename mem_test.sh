73c0389434a0ca27d87876f286b16057a0b7435e
Michael D. Catchen <mdcatchen@gmail.com>
2025-09-21 13:32:59 -0400

    full dataset in mem test

--- mem_test.sh
+++ mem_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes 1             
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=64G      
-#SBATCH --time=00:20:00
+#SBATCH --mem=48G      
+#SBATCH --time=00:30:00
 #SBATCH --output=%x-%j.out
 #SBATCH --job-name=InteractionTest
+#SBATCH --account=def-tpoisot
 
 module load python/3.13
 virtualenv --no-download $SLURM_TMPDIR/env
